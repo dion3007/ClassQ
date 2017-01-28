e5871fe7d538fa0e850e5478e076b4b9cdd4bf57
Leslie Aula <cristiano.ulo@gmail.com>
2017-01-28 18:51:16 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>ClassQ</title>
+    <head>
+    <body>
+        <div id="container">
+            <form action="system/register.php" method="post">
+                <input type="text" name="Firstname:">
+                <input type="text" name="Lastname:">
+                <input type="date" name="Date of Birth:">
+                <input type="email" name="Your E-mail:">
+                <input type="password" name="Enter New Password:">
+                <input type="password" name="Confirm Password:">
+                <input type="tel" name="Phone Number:">
+                <input type="submit" name="submit">
+            </form>
+        </div>
+    </body>
+</html>
